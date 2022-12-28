c55248c529bec6c65d612d66ac969dbb85968bd9
mffonua <108596346+mffonua@users.noreply.github.com>
2022-12-28 15:19:26 -0700

    added static file firmware and installed uuid

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,25 @@
 // import json data from db
 const fs = require("fs");
+const uuid = require('uuid')
+
 const noteData = require("../db/db.json")
 
 // console.log(noteData)
 
 module.exports = function(app) {
 
+     // route to get notes
     app.get("/api/notes", function(req,res) {
         res.json(noteData)
     });
+
+    // route to add a new note and add it to the json file
     app.post("/api/notes", function (req, res) {
-        let newNote = req.body;
+        let newNote = {
+            id: uuid(),
+            title: req.body.title,
+            text: req.body.text
+        };
         console.log(newNote);
     
         noteData.push(newNote);
@@ -19,10 +28,27 @@ module.exports = function(app) {
             if (err) {
               return console.log(err);
             }
-            console.log(noteData);
+           // console.log(noteData);
           }
         );
     
         res.send(noteData);
+      });
+
+      app.delete("/api/notes/:id", function(req,res) {
+
+        // console.log(req.body.title);
+        const noteToDelete = req.body.title;
+    
+        const newNoteData = {};
+    
+        for (let i = 0; i < noteData.length; i++) {
+            if (noteToDelete !== noteData[i].title) {
+                newNoteData.push()
+            }
+    
+        }
+    
+        res.send("note deleted")
       });
     };
